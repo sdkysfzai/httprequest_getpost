3089f6c210083d2f6fbbd081b9147ac05670ba78
silokilo966 <silokilo966@gmail.com>
2021-07-14 14:13:06 -0700

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_req_advanced/usermodel.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,6 +24,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var users;
+  Future<UserModel> createUser(String name, String job) async {
+    final apiUrl = "https://reqres.in/api/users";
+    final response =
+        await http.post(Uri.parse(apiUrl), body: {"name": name, "job": job});
+
+    if (response.statusCode == 201) {
+      users = userModelFromJson(response.body);
+    } else
+      throw Exception('Failed to load');
+    return users;
+  }
+
+  UserModel? user;
+
   final nameController = TextEditingController();
   final jobController = TextEditingController();
   @override
@@ -46,12 +60,35 @@ class _MyHomePageState extends State<MyHomePage> {
                 controller: jobController,
               ),
               ElevatedButton(
-                onPressed: null,
+                onPressed: () async {
+                  final String name = nameController.text;
+                  final String job = jobController.text;
+
+                  final UserModel userr = await createUser(name, job);
+
+                  setState(() {
+                    user = userr;
+                  });
+                },
                 child: Text('Make a Request'),
-              )
+              ),
+              user != null
+                  ? _showData(user!.name, user!.job, user!.id, user!.createdAt)
+                  : Container(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _showData(String name, String job, String id, DateTime createdat) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: SizedBox(
+        height: 32,
+        child:
+            Text('The user $name [$id] is created at $createdat with job $job'),
       ),
     );
   }
